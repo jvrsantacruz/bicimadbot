2558129953ca404c8f66f44af4b9b41d8532cece
Javier Santacruz <javier.santacruz.lc@gmail.com>
2015-08-26 22:23:42 +0200

    Adds 'real time' update processing via long polling

--- bicimad/telegram.py
+++ bicimad/telegram.py
@@ -94,19 +94,54 @@ def process_updates(updates, config, telegram, bicimad):
     config['telegram.offset'] = last_update
 
 
+def to_int(text):
+    try:
+        return int(text)
+    except (TypeError, ValueError):
+        return None
+
+
 class Telegram(object):
-    def __init__(self, host, token):
+    def __init__(self, host, token, timeout=None, poll_timeout=None):
         self.url = urljoin(host, '/bot' + token)
+        #: max time to wait for regular responses
+        self.timeout = 5 if timeout is None else timeout
+        #: max time to wait to the server to send data (see get_updates)
+        self.poll_timeout = 300 if poll_timeout is None else poll_timeout
 
     @classmethod
     def from_config(cls, config):
-        return cls(DEFAULT_HOST, config.get('telegram.token'))
+        return cls(DEFAULT_HOST, config.get('telegram.token'),
+                   timeout=to_int(config.get('telegram.timeout')),
+                   poll_timeout=to_int(config.get('telegram.poll_timeout')))
 
     def send_telegram(self, endpoint, **kwargs):
-        return requests.get(urljoin(self.url, endpoint), params=kwargs).json()
+        """Send generic telegram api requests"""
+        return requests.get(urljoin(self.url, endpoint),
+                            timeout=self.timeout, params=kwargs).json()
 
     def get_updates(self, offset=0):
-        return self.send_telegram('getUpdates', offset=offset)
+        """Get input updates from the server
+
+        It uses long polling to get the updates from the server without making
+        a request every so. Performs a long standing request that will resolve
+        either when timeout is out or when the server sends some results.
+
+        The ``.poll_timeout`` variable is used to know how much to wait for
+        results before starting a new request.
+
+        :param offset: Identifier of the last processed update
+        :returns: Response with 'result' containing list of updates.
+        """
+        # add some time to local timeout to allow the server to finish
+        # gracefully without suddently closing the current connection.
+        timeout = (self.timeout, self.poll_timeout + 1)
+
+        # Tell the server what to return and how much to wait
+        params = dict(offset=offset, timeout=self.poll_timeout)
+
+        return requests.get(urljoin(self.url, 'getUpdates'),
+                            timeout=timeout, params=params).json()
 
     def send_message(self, chat_id, text):
         return self.send_telegram('sendMessage', chat_id=chat_id, text=text)
