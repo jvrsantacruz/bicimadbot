620bc5b037ecafa76b97ebc3bbbe48b5f5e981af
Javier Santacruz <javier.santacruz.lc@gmail.com>
2015-08-26 22:34:54 +0200

    Include versioned production dependences

--- setup.py
+++ setup.py
@@ -1,6 +1,10 @@
-# -*- coding: utf-8 -*-
-
+import os
 from setuptools import setup, find_packages
+
+here = os.path.dirname(os.path.abspath(__file__))
+
+with open(os.path.join(here, 'requirements-fixed.txt')) as stream:
+    REQUIRES = stream.readlines()
 
 
 setup(
@@ -11,20 +15,14 @@ setup(
     author_email=u'javier.santacruz.lc@gmail.com',
     url=u'https://github.com/jvrsantacruz/bicimad',
     packages=find_packages(exclude=[u'tests', u'tests.*']),
-    install_requires=[
-        u'click',
-        u'geopy',
-        u'bottle',
-        u'requests',
-    ],
+    zip_safe=False,
+    include_package_data=True,
+    install_requires=REQUIRES,
     classifiers=[
         u'Environment :: Console',
         u'Operating System :: POSIX',
         u'Development Status :: 3 - Alpha',
         u'Programming Language :: Python',
-        u'Programming Language :: Python :: 2',
-        u'Programming Language :: Python :: 2.7',
-        u'Programming Language :: Python :: 3.3',
         u'Programming Language :: Python :: 3.4',
         u'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
     ],
