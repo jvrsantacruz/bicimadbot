57e45efe085cdf81e0d31ff32a27a66e615aa5a7
Javier Santacruz <javier.santacruz.lc@gmail.com>
2015-08-31 21:15:24 +0200

    Bumps version 4.0.1

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ with open(os.path.join(here, 'requirements-fixed.txt')) as stream:
 
 setup(
     name=u'bicimad',
-    version=u'4.0.0',
+    version=u'4.0.1',
     description=u'BiciMad unofficial api',
     author=u'Javier Santacruz',
     author_email=u'javier.santacruz.lc@gmail.com',
