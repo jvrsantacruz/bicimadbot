0e3475e6a4dd40c092fc7eba086184a2374bba0d
Javier Santacruz <javier.santacruz.lc@gmail.com>
2015-09-06 12:51:24 +0200

    Uses regular strings instead of u'' unicode strings in setup.py

--- setup.py
+++ setup.py
@@ -8,26 +8,26 @@ with open(os.path.join(here, 'requirements-fixed.txt')) as stream:
 
 
 setup(
-    name=u'bicimad',
-    version=u'4.2.3',
-    description=u'BiciMad unofficial api',
-    author=u'Javier Santacruz',
-    author_email=u'javier.santacruz.lc@gmail.com',
-    url=u'https://github.com/jvrsantacruz/bicimad',
-    packages=find_packages(exclude=[u'tests', u'tests.*']),
+    name='bicimad',
+    version='4.2.3',
+    description='BiciMad unofficial api',
+    author='Javier Santacruz',
+    author_email='javier.santacruz.lc@gmail.com',
+    url='https://github.com/jvrsantacruz/bicimad',
+    packages=find_packages(exclude=['tests', 'tests.*']),
     zip_safe=False,
     include_package_data=True,
     install_requires=REQUIRES,
     classifiers=[
-        u'Environment :: Console',
-        u'Operating System :: POSIX',
-        u'Development Status :: 3 - Alpha',
-        u'Programming Language :: Python',
-        u'Programming Language :: Python :: 3.4',
-        u'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
+        'Environment :: Console',
+        'Operating System :: POSIX',
+        'Development Status :: 3 - Alpha',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3.4',
+        'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
     ],
-    platforms=[u'Any'],
+    platforms=['Any'],
     entry_points={
-        u'console_scripts': [u'bmad = bicimad.cli:cli']
+        'console_scripts': ['bmad = bicimad.cli:cli']
     }
 )
